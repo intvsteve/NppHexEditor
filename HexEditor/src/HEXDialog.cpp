5e49e26ad7ca48ff77326dac468f9061359e5b2a
Unknown <github@intvfunhouse.com>
2020-01-05 22:49:29 -0600

    Fix calls to std::vector.erase() to use proper iterator args

--- HexEditor/src/HEXDialog.cpp
+++ HexEditor/src/HEXDialog.cpp
@@ -3870,7 +3870,7 @@ void HexEdit::ToggleBookmark(UINT iItem)
 	{
 		if (_pCurProp->vBookmarks[i].iItem == iItem) {
 			/* if bookmark exists delete it from list */
-			_pCurProp->vBookmarks.erase(&_pCurProp->vBookmarks[i]);
+			_pCurProp->vBookmarks.erase(_pCurProp->vBookmarks.begin() + i);
 			isChanged = TRUE;
 		}
 		else if (_pCurProp->vBookmarks[i].iItem > iItem) {
@@ -3915,7 +3915,7 @@ void HexEdit::UpdateBookmarks(UINT firstAdd, INT length)
 
 			if ((_pCurProp->vBookmarks[i].lAddress == firstAdd) && (length < 0)) {
 				/* remove bookmark if is in a delete section */
-				_pCurProp->vBookmarks.erase(&_pCurProp->vBookmarks[i]);
+				_pCurProp->vBookmarks.erase(_pCurProp->vBookmarks.begin() + i);
 				i--;
 			} else if ((UINT)_pCurProp->vBookmarks[i].lAddress > firstAdd) {
 				/* calculate new addresses of bookmarks behind the first address */
@@ -3927,7 +3927,7 @@ void HexEdit::UpdateBookmarks(UINT firstAdd, INT length)
 
 				/* if some data was deleted and the changed item matches with the previous one delete it */
 				if ((i != 0) && (_pCurProp->vBookmarks[i].lAddress == _pCurProp->vBookmarks[i-1].lAddress)) {
-					_pCurProp->vBookmarks.erase(&_pCurProp->vBookmarks[i]);
+					_pCurProp->vBookmarks.erase(_pCurProp->vBookmarks.begin() + i);
 					i--;
 				}
 
