ed625950e1470e4fc8628a2822ea79127199423b
Unknown <github@intvfunhouse.com>
2020-01-06 21:36:02 -0600

    Update ImageListSet
    Some small tweaks for C++ features not supported in VS2012

--- Common/Ccpp/ImageListSet.h
+++ Common/Ccpp/ImageListSet.h
@@ -1,26 +1,36 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO ( donho@altern.org )
+// This file is part of Notepad++ project
+// Copyright (C)2003 Don HO <don.h@free.fr>
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// Note that the GPL places important restrictions on "derived works", yet
+// it does not provide a detailed definition of that term.  To avoid      
+// misunderstandings, we consider an application to constitute a          
+// "derivative work" for the purpose of this license if it does any of the
+// following:                                                             
+// 1. Integrates source code from Notepad++.
+// 2. Integrates/includes/aggregates Notepad++ into a proprietary executable
+//    installer, such as those produced by InstallShield.
+// 3. Links to a library or executes a program that does any of the above.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-#ifndef IMAGE_LIST_H
-#define IMAGE_LIST_H
 
+#pragma once
+
+#include <vector>
 #include <windows.h>
 #include <commctrl.h>
-#include <vector>
 
 const int nbMax = 45;
 #define	IDI_SEPARATOR_ICON -1
@@ -28,61 +38,24 @@ const int nbMax = 45;
 class IconList
 {
 public :
-	IconList() : _hImglst(NULL) {};
-
-	void create(HINSTANCE hInst, int iconSize) {
-		InitCommonControls();
-		_hInst = hInst;
-		_iconSize = iconSize; 
-		_hImglst = ImageList_Create(iconSize, iconSize, ILC_COLOR32 | ILC_MASK, 0, nbMax);
-		if (!_hImglst)
-			throw int(25);
-	};
-
-	void create(int iconSize, HINSTANCE hInst, int *iconIDArray, int iconIDArraySize) {
-		create(hInst, iconSize);
-		_pIconIDArray = iconIDArray;
-		_iconIDArraySize = iconIDArraySize;
-
-		for (int i = 0 ; i < iconIDArraySize ; i++)
-			addIcon(iconIDArray[i]);
-	};
+	IconList() : _hImglst(NULL) {} // = default;
+	void create(HINSTANCE hInst, int iconSize);
+	void create(int iconSize, HINSTANCE hInst, int *iconIDArray, int iconIDArraySize);
 
 	void destroy() {
 		ImageList_Destroy(_hImglst);
 	};
-
 	HIMAGELIST getHandle() const {return _hImglst;};
+	void addIcon(int iconID) const;
+	bool changeIcon(int index, const TCHAR *iconLocation) const;
+	void setIconSize(int size) const;
 
-	void addIcon(int iconID) const {
-		HICON hIcon = ::LoadIcon(_hInst, MAKEINTRESOURCE(iconID));
-		if (!hIcon)
-			throw int(26);
-		ImageList_AddIcon(_hImglst, hIcon);
-		::DeleteObject(hIcon);
-	};
-
-	bool changeIcon(int index, const TCHAR *iconLocation) const{
-		HBITMAP hBmp = (HBITMAP)::LoadImage(_hInst, iconLocation, IMAGE_ICON, _iconSize, _iconSize, LR_LOADFROMFILE | LR_LOADMAP3DCOLORS | LR_LOADTRANSPARENT);
-		if (!hBmp)
-			return false;
-		int i = ImageList_ReplaceIcon(_hImglst, index, (HICON)hBmp);
-		ImageList_AddMasked(_hImglst, (HBITMAP)hBmp, RGB(255,0,255));
-		::DeleteObject(hBmp);
-		return (i == index);
-	};
-
-	void setIconSize(int size) const {
-		ImageList_SetIconSize(_hImglst, size, size);
-		for (int i = 0 ; i < _iconIDArraySize ; i++)
-			addIcon(_pIconIDArray[i]);
-	};
 private :
-	HIMAGELIST _hImglst;
-	HINSTANCE _hInst;
-	int *_pIconIDArray;
-	int _iconIDArraySize;
-	int _iconSize;
+	HIMAGELIST _hImglst = nullptr;
+	HINSTANCE _hInst = nullptr;
+	int *_pIconIDArray = nullptr;
+	int _iconIDArraySize = 0;
+	int _iconSize = 0;
 };
 
 typedef struct 
@@ -94,8 +67,7 @@ typedef struct
 	int _grayIcon;
 
 	int _stdIcon;
-	UINT _uIconStyle;
-}ToolBarButtonUnit;
+} ToolBarButtonUnit;
 
 typedef std::vector<ToolBarButtonUnit> ToolBarIconIDs;
 
@@ -104,7 +76,7 @@ typedef std::vector<IconList> IconListVector;
 class IconLists
 {
 public :
-	IconLists() {};
+	IconLists() {} // = default;
 	HIMAGELIST getImageListHandle(int index) const {
 		return _iconListVector[index].getHandle();
 	};
@@ -120,7 +92,7 @@ const int HLIST_DISABLE = 2;
 class ToolBarIcons : public IconLists
 {
 public :
-	ToolBarIcons() : _nbCmd(0) {};
+	ToolBarIcons() : _nbCmd(0) {} // = default;
 
 	void init(ToolBarButtonUnit *buttonUnitArray, int arraySize);
 	void create(HINSTANCE hInst, int iconSize);
@@ -143,22 +115,7 @@ public :
 		reInit(size);
 	};
 
-	void reInit(int size) {
-		ImageList_SetIconSize(getDefaultLst(), size, size);
-		ImageList_SetIconSize(getHotLst(), size, size);
-		ImageList_SetIconSize(getDisableLst(), size, size);
-
-		for (int i = 0 ; i < int(_tbiis.size()) ; i++)
-		{
-			if (_tbiis[i]._defaultIcon != -1)
-			{
-				_iconListVector[HLIST_DEFAULT].addIcon(_tbiis[i]._defaultIcon);
-				_iconListVector[HLIST_HOT].addIcon(_tbiis[i]._hotIcon);
-				_iconListVector[HLIST_DISABLE].addIcon(_tbiis[i]._grayIcon);
-			}
-		}
-
-	};
+	void reInit(int size);
 
 	int getNbIcon() const {
 		return int(_tbiis.size());
@@ -168,21 +125,14 @@ public :
 		return _tbiis[i]._stdIcon;
 	};
 
-	UINT getIconStyle(int i) const {
-		return _tbiis[i]._uIconStyle;
-	};
-
-
 	bool replaceIcon(int witchList, int iconIndex, const TCHAR *iconLocation) const {
 		if ((witchList != HLIST_DEFAULT) && (witchList != HLIST_HOT) && (witchList != HLIST_DISABLE))
 			return false;
 		return _iconListVector[witchList].changeIcon(iconIndex, iconLocation);
-		
 	};
 
 private :
 	ToolBarIconIDs _tbiis;
-	unsigned int _nbCmd;
+	unsigned int _nbCmd = 0;
 };
 
-#endif //IMAGE_LIST_H
